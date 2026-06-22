d82a01566e2fc9b1c7b50c803d4ba61e6e10547f
QuocToan <164734315+quoctoan07-vn@users.noreply.github.com>
2026-06-22 22:13:33 +0700

    Create QUOCTOANDEV.cpp

---
+++ QUOCTOANDEV.cpp
@@ -0,0 +1,16 @@
+#include "QUOCTOANDEV.h"
+
+_lxy__size_t& _lxy_qtd_any_::X() {
+    static _lxy__size_t x = 0;
+    return x;
+}
+
+_lxy__size_t& _lxy_qtd_any_::Y() {
+    static _lxy__size_t y = 0;
+    return y;
+}
+
+_lxy__size_t& _lxy_qtd_any_::Z() {
+    static _lxy__size_t z = 0;
+    return z;
+}
